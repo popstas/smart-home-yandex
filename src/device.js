6cec60ad0421b5389faaaadf11bf971cfcc1c830
Stanislav Popov <popstas@gmail.com>
2020-05-12 00:01:47 +0500

    fix: уникальные id устройств по именам и расположению, исправление ошибок, связанных с переключением состояния

--- src/device.js
+++ src/device.js
@@ -1,9 +1,10 @@
+const md5 = require('md5');
+
 class device {
   constructor(options) {
-    var id = global.devices.length;
     // this.client = client;
     this.data = {
-      id: String(id),
+      id: '',
       name: options.name || 'Без названия',
       description: options.description || '',
       type: options.type || 'devices.types.light',
@@ -21,7 +22,9 @@ class device {
           }
         }
       ]
-    }
+    };
+
+    this.data.id = md5(this.data.name + this.data.room);
     global.devices.push(this);
   }
   getInfo() {
@@ -30,23 +33,19 @@ class device {
   setState(val) {
     const s = val ? 'on' : 'off';
     const int = val ? '1' : '0';
-    this.data.capabilities[0].state.instance = s;
+    // this.data.capabilities[0].state.instance = s;
     this.data.capabilities[0].state.value = val;
     const topic = this.data.custom_data.mqtt.set || false;
     if (topic) {
       console.log(`mqtt: ${topic} ${s}`);
       this.client.publish(topic, int);
     }
-    return [
-      {
-        'state': {
-          'instance': s,
-          'action_result': {
-            'status': 'DONE'
-          }
-        }
-      }
-    ];
+
+    const capabilities = this.data.capabilities;
+    capabilities[0].state.action_result = {
+      status: 'DONE'
+    };
+    return capabilities;
   }
 }
 module.exports = device;
--- src/providerAdapter.js
+++ src/providerAdapter.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const getDeviceById = id => {
+  return global.devices.find(dev => dev.data.id === id);
+}
+
 router.all('/', (req, res) => {
   // console.log('ping');
   res.send('OK');
@@ -20,8 +24,8 @@ router.get('/v1.0/user/devices', (req, res) => {
       devices: []
     }
   };
-  for (var i in global.devices) {
-    r.payload.devices.push(global.devices[i].getInfo());
+  for (let device of global.devices) {
+    r.payload.devices.push(device.getInfo());
   }
   res.send(r);
 });
@@ -34,8 +38,10 @@ router.post('/v1.0/user/devices/query', (req, res) => {
       devices: []
     }
   };
-  for (let i in req.body.devices) {
-    r.payload.devices.push(global.devices[req.body.devices[i].id].getInfo());
+  for (let requestDevice of req.body.devices) {
+    const device =getDeviceById(requestDevice.id);
+    if(!device) continue;
+    r.payload.devices.push(device.getInfo());
     // console.log(global.devices[req.body.devices[i].id].getInfo());
   }
   // console.log(JSON.stringify(r));
@@ -45,16 +51,19 @@ router.post('/v1.0/user/devices/query', (req, res) => {
 router.post('/v1.0/user/devices/action', (req, res) => {
   console.log('/v1.0/user/devices/action:', req.body.payload.devices);
   // console.log('global.devices: ', global.devices);
-  var r = {
+  const r = {
     request_id: req.headers['x-request-id'],
     payload: {
       devices: []
     }
   };
-  for (var i in req.body.payload.devices) {
-    var id = req.body.payload.devices[i].id;
-    var capabilities = global.devices[id].setState(req.body.payload.devices[i].capabilities[0].state.value);
-    r.payload.devices.push({ id: id, capabilities: capabilities });
+  for (let requestDevice of req.body.payload.devices) {
+    const device = getDeviceById(requestDevice.id);
+    if(!device) continue;
+
+    const id = device.data.id;
+    const capabilities = device.setState(requestDevice.capabilities[0].state.value);
+    r.payload.devices.push({ id, capabilities });
   }
   res.send(r);
 });
